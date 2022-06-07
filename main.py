38147a8c633be5d2acf8f4916c3166b7dfbbe004
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2022-06-07 19:35:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,49 @@
 import os
 import requests
-from pyromod import listen
-from requests.structures import CaseInsensitiveDict
-from pyrogram import filters, Client
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import *
-from pyrogram.errors import (
-    SessionPasswordNeeded, FloodWait,
-    PhoneNumberInvalid, ApiIdInvalid,
-    PhoneCodeInvalid, PhoneCodeExpired
-)
+import asyncio
+import discord
+from discord.ext import commands
 
-API_ID = os.environ.get('API_ID')
-API_HASH = os.environ.get('API_HASH')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-NEXT_OFFSET = 25
-CACHE_TIME = 0
+client = commands.Bot(command_prefix='!')
 
-bot = Client('bot',
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=50,
-             sleep_threshold=10)
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-
-@bot.on_message(filters.command('info') & filters.private)
-async def start(bot, message):
-    i_d = await bot.ask(
-        message.chat.id,
-        f"Hello **{message.chat.first_name}!**\n"
-        "Send The Narayana Admission Number (NAN) of the student you want to get information about.\n",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Support", url="https://t.me/yssprojects"),
-                    InlineKeyboardButton(
-                        "Developer", url="https://t.me/seshu2004")
-                ]
-            ]
-        )
-    )
-    id = i_d.text
-    if len(id) == 7:
-        msg = await bot.send_message(message.chat.id, "Getting `{}` Information".format(i_d.text))
-        req = requests.get(
-            "https://npayapi-in.herokuapp.com/get?id={}".format(id))
-        if req.status_code == 200:
-            data = req.json()
-            if data["status"] == True:
-                text = f"|--**Here The Details Of {id}**--\n|->**Name**: `{data['name']}`\n|->**Father Name**: `{data['father_name']}`\n|->**Admin No**: `{data['id_no']}`\n|->**Class**: `{data['class_n']}`\n|->**Branch**: `{data['branch']}`\n|->**Due Amount**: `{data['due_amount']}`\n|->**Mobile Number**: || `{data['mobile']}` ||\n|(Created By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/))"
-                await msg.delete()
-                await bot.send_message(message.chat.id, text, reply_to_message_id=i_d.id, protect_content=True,
-                                       reply_markup=InlineKeyboardMarkup(
-                                           [
-                                               [
-                                                   InlineKeyboardButton(
-                                                       "Support", url="https://t.me/yssprojects"),
-                                                   InlineKeyboardButton(
-                                                       "Developer", url="https://t.me/seshu2004")
-                                               ]
-                                           ]
-                                       )
-                                       )
-            else:
-                await msg.edit_text("I'm Sorry, I Can't Find Your Information \nPlease Try Again Using /info Command\n/help For More Details")
+def get_text(id):
+    url = f"https://npayapi-in.herokuapp.com/get?id={id}"
+    req = requests.get(url)
+    if req.status_code == 200:
+        data = req.json()
+        if data["status"] == True:
+            text = f"|--**Here The Details Of {id}**--\n|->**Name**: `{data['name']}`\n|->**Father Name**: `{data['father_name']}`\n|->**Admin No**: `{data['id_no']}`\n|->**Class**: `{data['class_n']}`\n|->**Branch**: `{data['branch']}`\n|->**Due Amount**: `{data['due_amount']}`\n|->**Mobile Number**: || `{data['mobile']}` ||\nCreated By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/)"
+            return text
         else:
-            await bot.send_message(message.chat.id, "Please Enter A Valid I'd")
+            return f"**No Details Found For {id}**\nCreated By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/)"
+    else:
+        return f"**No Details Found For {id}**\nCreated By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/)"
 
 
-@bot.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await bot.send_message(message.chat.id,
-                           reply_to_message_id=message.id,
-                           text=f"Hello **{message.chat.first_name}!**\nI'm **Narayana Sudent Information Spoiler Bot**\nI'll Get Information of Narayana Student Using Admission Number\nTo Get Information Use /info\nFor More Use /help\nI'm Created By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/)",
-                           reply_markup=InlineKeyboardMarkup(
-                               [
-                                   [
-                                       InlineKeyboardButton(
-                                           "Support", url="https://t.me/yssprojects"),
-                                       InlineKeyboardButton(
-                                           "Developer", url="https://t.me/seshu2004")
-                                   ]
-                               ]
-                           )
-                           )
 
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
 
-@bot.on_message(filters.command('help') & filters.private)
-async def start(bot, message):
-    await bot.send_message(message.chat.id,
-                           reply_to_message_id=message.id,
-                           text=f"Hello **{message.chat.first_name}!**\nI'll Get Information of Narayana Student Using Admission Number\nTo Get Information Use /info\n Your Admission Number Must Me 7 Digits And Number\nI'm Created By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/)",
-                           reply_markup=InlineKeyboardMarkup(
-                               [
-                                   [
-                                       InlineKeyboardButton(
-                                           "Support", url="https://t.me/yssprojects"),
-                                       InlineKeyboardButton(
-                                           "Developer", url="https://t.me/seshu2004")
-                                   ]
-                               ]
-                           )
-                           )
+@client.event
+async def on_message(message):
+    if message.content.startswith('!info'):
+        text = message.content.replace('!info ','')
+        if len(text) == 7:
+            tex = get_text(int(text))
+            await message.channel.send(tex)
+        else:
+            await message.channel.send("**Invalid ID**\nCreated By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/")
+    elif message.content.startswith('!help'):
+        await message.channel.send("I'll Get Information of Narayana Student Using Admission Number\nTo Get Information Use /info\n Your Admission Number Must Me 7 Digits And Number\nI'm Created By [Seshu Sai](https://www.instagram.com/_yarra.s.s_/)")
 
-if __name__ == "__main__":
-    bot.run()
+TOKEN = os.environ.get('BOT_TOKEN')
+
+client.run(TOKEN)
